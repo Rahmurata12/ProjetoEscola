9c0b37b421b01a30718ba3fc1918815ed6b6b8c5
Rahmurata12 <26190277+Rahmurata12@users.noreply.github.com>
2022-05-03 06:46:17 -0300

    Add files via upload

--- CodigoJava/dao/dao/AlunoDAO.java
+++ CodigoJava/dao/dao/AlunoDAO.java
@@ -1,4 +1,5 @@
 package model.dao;
+
 import connection.ConnectionDB;
 import model.bean.Aluno;
 
@@ -9,34 +10,42 @@ import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
 public class AlunoDAO {
-	
+
 	public void create(Aluno al) {
-		
+
 		Connection con = ConnectionDB.getConnection();
-		PreparedStatement stmt = null;
-		
+		PreparedStatement stmtAl = null;
+		PreparedStatement stmtPes = null;
+
 		try {
+
+			stmtAl = con.prepareStatement(
+					"INSERT INTO sistemagestaoescola.pessoa (nome, dt_Nascimento, RG, CPF) VALUES (?,?, ?, ?);");
+			stmtAl.setString(1, al.getNome());
+			stmtAl.setString(2, al.getDt_Nascimento());
+			stmtAl.setString(3, al.getRG());
+			stmtAl.setString(4, al.getCPF());
 			
-			stmt = con.prepareStatement("INSERT INTO sistemagestaoescola.aluno (nome,dt_Nascimento,RG,CPF,nome_mae,nome_pai,cep,endereco,complemento) VALUES(?,?,?,?,?,?,?,?,?)");
-			stmt.setString(1, al.getNome());
-			stmt.setString(2, al.getDt_Nascimento());
-			stmt.setString(3, al.getRG());
-			stmt.setString(4, al.getCPF());
-			stmt.setString(5, al.getNomeMae());
-			stmt.setString(6, al.getNomePai());
-			stmt.setString(7, al.getCep());
-			stmt.setString(8, al.getEndereco());
-			stmt.setString(9, al.getComplemento());
+			stmtPes = con.prepareStatement("INSERT INTO sistemagestaoescola.aluno  (nome_mae,nome_pai,cep,endereco,complemento,pessoa_Cd_Pessoa) VALUES (?,?,?,?,?,?);");
+			stmtPes.setString(1, al.getNomeMae());
+			stmtPes.setString(2, al.getNomePai());
+			stmtPes.setString(3, al.getCep());
+			stmtPes.setString(4, al.getEndereco());
+			stmtPes.setString(5, al.getComplemento());
+			stmtPes.setInt(6, 10);
 			
-			
-			stmt.executeUpdate();
+			stmtAl.executeUpdate();
+			stmtPes.executeUpdate();
 			
 			JOptionPane.showMessageDialog(null, "Salvo!");
 		} catch (SQLException e) {
 			JOptionPane.showMessageDialog(null, "Erro!" + e);
 			e.printStackTrace();
 		} finally {
-			ConnectionDB.closeConnection(con, stmt);
+			ConnectionDB.closeConnection(con, stmtAl);
+			ConnectionDB.closeConnection(con, stmtPes);
 		}
 	}
+	
+	
 }
--- CodigoJava/dao/dao/TelefoneDAO.java
+++ CodigoJava/dao/dao/TelefoneDAO.java
@@ -2,7 +2,9 @@ package model.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
 
@@ -19,9 +21,10 @@ public class TelefoneDAO {
 		try {
 
 			stmt = con.prepareStatement(
-					"INSERT INTO sistemagestaoescola.pessoa (tipo_Tel,numero) VALUES(?,?)");
-			stmt.setString(1, tel.getTipo_Tel());
+					"INSERT INTO sistemagestaoescola.telefone (tipo_Tel,numero,pessoa_Cd_Pessoa) VALUES(?,?,?)");
+			stmt.setString(1, tel.getTipoTel());
 			stmt.setString(2, tel.getNumero());
+			stmt.setInt(3, 10);
 
 			stmt.executeUpdate();
 
@@ -33,5 +36,32 @@ public class TelefoneDAO {
 			ConnectionDB.closeConnection(con, stmt);
 		}
 	}
+	
+	public ArrayList<Telefone> listar() {
+
+		ArrayList<Telefone> tel = new ArrayList<>();
+		String read = "SELECT * FROM aulas.materias_professores";
+
+		try {
+			Connection con = ConnectionDB.getConnection();
+			PreparedStatement stmt = null;
+			stmt = con.prepareStatement(
+					"SELECT * FROM aulas.materias_professores");
+			ResultSet rs = stmt.executeQuery();
+
+			while (rs.next()) {
+				String tipoTel = rs.getString(2);
+				String numero = rs.getString(3);
+
+				tel.add(new Telefone(tipoTel, numero));
+			}
+			con.close();
+			return tel;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+	
 
 }
